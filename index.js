5884a08eeaf572324cfbf1b0783f9c7391236282
Kapri27 <paumau27@gmail.com>
2025-08-31 03:36:51 -0400

    los endpoint get id y post mascota ya funcionan correctamente. se hace console en servidor y en la consola del front

--- index.js
+++ index.js
@@ -1,85 +1,77 @@
 //IMPORTAMOS EXPRESS Y LA GUARDAMOS EN LA VARIABLE LLAMADA EXPRESS
-const express = require ("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
 //CREAMOS LA APLICACIÓN CON EXPRESS
-const app = express()
-app.use(cors())
-app.use(express.json())
-
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("hola Paulita")
-})
+  res.send(mascotaJugador + " es tu mascota");
+});
 //ARRAYJUGADORES
-const jugadores = []
+const jugadores = [];
 
 //CLASE CONSTRUCTORA JUGADOR
 class Jugador {
-    constructor(id){
-    this.id = id
-    }
-    
-//     //asignar mokepon
-//     asignarMokepon(mokepon){
-//         this.mokepon = mokepon
-//     }
+  constructor(id) {
+    this.id = id;
+  }
 
-//     // actualizarPosicion(x, y){
-//     //     this.x = x  
-//     //     this.y = y
-//     // }
+  //     //asignar mokepon
+  //     asignarMokepon(mokepon){
+  //         this.mokepon = mokepon
+  //     }
+
+  //     // actualizarPosicion(x, y){
+  //     //     this.x = x
+  //     //     this.y = y
+  //     // }
 }
 
 //SE LE AGREGARA UN MOKEPON AL JUGADOR CUANDO SE RECIBA LA INFORMACIÓN
 class Mokepon {
-    constructor(nombre){
-        this.nombre = nombre
-        }   
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
 }
 
 //SOLICITUD A LA APP, LE ASIGNAREMOS UN ID RANDOM A CADA JUGADOR
 app.get("/unirse", (req, res) => {
+  //LE ASIGNAMOS UN ID RANDOM AL BUENO JUGADOR
+  const id = `${Math.random()}`;
+  //CREAMOS EL JUGADOR CON EL NUEVO id
+  const jugador = new Jugador(id);
+  //AGREGAMOS EL JUGADOR A LA LISTA DE JUGADORES
 
-// //LE ASIGNAMOS UN ID RANDOM AL BUENO JUGADOR
-    const id = `${Math.random()}` 
-
-    //CREAMOS EL JUGADOR CON EL NUEVO id
-    const jugador = new Jugador(id)
-
-    //AGREGAMOS EL JUGADOR A LA LISTA DE JUGADORES
-    jugadores.push(jugador)
-
-    //para permitir las peticiones desde cualquier origen
-    // res.setHeader("Access-Control-Allow-Origin", "*")
-
-    res.send(id)
-    console.log("back envía el jugadorId, es : " + id)
-})
+  jugadores.push(jugador);
+  res.send(id); // ESTO ES LO QUE SE ENVÍA A FRONTEN Y TAMBN LO QUE SE RENDERIZA EN LA PAGINA WEB
+  console.log("back te asigno este id = " + id); // esto lo mostrará en el servidor
+});
 
 //URL ID JUGADOR
 app.post("/mokepon/:jugadorId", (req, res) => {
-    const jugadorId = req.params.jugadorId || ""
-    const nombre = req.body.mokepon 
-    const mokepon = new Mokepon(nombre)
-    
-    // const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+  const jugadorId = req.params.jugadorId || "";
+  const nombre = req.body.mokepon;
+  const mokepon = new Mokepon(nombre);
 
-    // if(jugadorIndex >= 0){
-    //     jugadores[jugadorIndex].asignarMokepon(mokepon)
-    // } 
-    // console.log(jugadores + " Backend envia jugadores")
-    console.log("Back envía el jugadorId, es = " +jugadorId)
-    console.log("Backend recibe mascota es : " + mokepon.nombre)
-    res.end()
-})
+  // const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
 
-//ENVIAR POSICION DE MOKEPON
+  // if(jugadorIndex >= 0){
+  //     jugadores[jugadorIndex].asignarMokepon(mokepon)
+  // }
+  // console.log(jugadores + " Backend envia jugadores")
+  console.log("Elegiste a : " + mokepon.nombre);
+  res.end();
+});
+
+//ENVIAR POSICION DE MOKEPONY
 // app.post(`/mokepon/:jugadorId/posicion`, (req, res) => {
 //         // console.log("Llegó petición a /posicion", req.params, req.body)
 //     const jugadorId = req.params.jugadorId || ""
 //     const x = req.body.x || 0
-//     const y = req.body.y || 0 
+//     const y = req.body.y || 0
 //     const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
 
 //     if(jugadorIndex >= 0){
@@ -90,5 +82,5 @@ app.post("/mokepon/:jugadorId", (req, res) => {
 
 //ESCUCHANDO EL SERVIDOR
 app.listen(8080, () => {
-    console.log("el servidor está sirviendo")
-})
+  console.log("el servidor está sirviendo");
+});
